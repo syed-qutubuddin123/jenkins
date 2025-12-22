3c3c5eb7fea4f33d80fbc9aa9d3d91964ed5ec0c
syed-qutubuddin123 <syedqutubuddin397@gmail.com>
2025-12-22 11:23:41 +0000

    terraform

--- variables.tf
+++ variables.tf
@@ -1,5 +1,20 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "subnet_id" {}
-variable "name" {}
+variable "ami_id" {
+  default = "ami-02eb7a4783e7e9317"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  default = "FC-key"
+}
+
+variable "subnet_id" {
+  default = "subnet-0ec7bc905d0373b2c"
+}
+
+variable "name" {
+  default = "terraform-ec2"
+}
+
