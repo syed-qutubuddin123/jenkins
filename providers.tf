4eb1a555be853c8e093719bdce8779fa1f3313e2
syed-qutubuddin123 <syedqutubuddin397@gmail.com>
2025-12-24 17:49:23 +0000

    terraform-jenkins

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "farhan-bucket-terraform"
+    bucket = "farhan-terraform-jenkins"
     key    = "env/terraform.tfstate"
     region = "ap-south-1"
   }
@@ -9,3 +9,4 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+
