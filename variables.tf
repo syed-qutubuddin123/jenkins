dc0e24e80b1eae254c73dbf32d1aa43196609903
syed-qutubuddin123 <syedqutubuddin397@gmail.com>
2025-12-29 13:43:36 +0000

    jenkins

--- variables.tf
+++ variables.tf
@@ -1,6 +1,18 @@
-variable "environment" {}
+variable "environment" {
+  type = string
+}
 
-variable "vpc_cidr" {}
+variable "vpc_name" {
+  type = string
+}
+
+variable "alb_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
 
 variable "public_subnets" {
   type = map(object({
@@ -14,5 +26,17 @@ variable "private_subnets" {
     cidr = string
     az   = string
   }))
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "zone_name" {
+  type = string
+}
+
+variable "server_records" {
+  type = map(string)
 }
 
