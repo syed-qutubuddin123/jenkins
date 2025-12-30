adde5e93962f1fdac5e346b3d877396da6d7384d
syed-qutubuddin123 <syedqutubuddin397@gmail.com>
2025-12-30 18:18:08 +0000

    jenkins-project

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -38,10 +38,10 @@ resource "aws_route_table" "private_route" {
 resource "aws_subnet" "public" {
   for_each = var.public_subnets
 
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = each.value.cidr
-  availability_zone = each.value.az
-
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = each.value.cidr
+  availability_zone       = each.value.az
+  map_public_ip_on_launch = true
   tags = {
     Name = "${var.vpc_name}-${each.key}"
   }
